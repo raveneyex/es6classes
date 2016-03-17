e09315d6a1dfed9972dab82dda68cd7cae60e52a
raveneyex <raveneyex@gmail.com>
2016-03-17 02:48:55 -0500

    [☢] Sub-Classing and Prototype chains

--- js/examples/presentation-examples.js
+++ js/examples/presentation-examples.js
@@ -132,3 +132,137 @@
   const rei = new Human('Rei Toei');
   console.log(rei.identify());
 }());
+
+(()=>{
+  /*  TAG 7 */
+  'use strict'
+
+  class Human {
+    constructor(name){
+      this._name = name;
+    }
+
+    identify() {
+      return `Hello, I'm ${this._name} and I'm a ${Human.name}`;
+    }
+  }
+  class Cyborg extends Human {
+    constructor(name){
+      super(name);
+    }
+
+    identify() {
+      return `Hello, I'm ${this._name} and I'm a ${Cyborg.name}`;
+    }
+  }
+  const t1000 = new Cyborg("Arnold");
+  console.log(t1000.identify());
+  console.log(t1000 instanceof Cyborg);
+  console.log(t1000 instanceof Human);
+}());
+
+(()=>{
+  /*  TAG 8 */
+  'use strict'
+
+  function Human(name) {
+      this._name = name;
+      this.identify = function() {
+        return `Hello, I'm ${this._name} and I'm a ${Human.name}`;
+      };
+  };
+  class Cyborg extends Human {
+    constructor(name){
+      super(name);
+    }
+  }
+  const t1000 = new Cyborg("Arnold");
+  console.log(t1000 instanceof Cyborg);
+  console.log(t1000 instanceof Human);
+}());
+
+(()=>{
+  /*  TAG 9 */
+  'use strict'
+
+  class Human {
+    constructor(name){
+      this._name = name;
+    }
+
+    identify() {
+      return `Hello, I'm ${this._name} and I'm a ${Human.name}`;
+    }
+  }
+  class Cyborg extends Human {
+    constructor(name, skill){
+      //this._skill = skill; //ReferenceError
+      super(name);
+      this._skill = skill;
+    }
+
+    identify() {
+      return `Hello, I'm ${this._name} and I'm a ${Cyborg.name}`;
+    }
+  }
+  const molly = new Cyborg('Molly Millions', 'Hypervision');
+  console.log(molly._skill);
+}());
+
+(()=>{
+  /*  TAG 10 */
+  'use strict'
+
+  class CustomError extends Error {
+  };
+  throw new CustomError("Ups! we're fucked!");
+}());
+
+(()=>{
+  /*  TAG 11 */
+  'use strict'
+  //Cyborg extends Hybrid, Human
+  class Human{
+    constructor(name) {
+      this._name = name;
+    }
+  }
+  const Hybrid = (superClass) => class extends superClass {
+      isHybrid() {
+        return 'I´m a hybrid';
+      }
+  };
+  class Cyborg extends Hybrid(Human) {
+    identify () {
+      return `Hello, I´m ${this._name}, and ${this.isHybrid()}`;
+    }
+  }
+  const jonny = new Cyborg("Jonny");
+  console.log(jonny._name);
+  console.log(jonny.isHybrid());
+  console.log(jonny.identify());
+  console.log(jonny instanceof Human);
+  console.log(jonny instanceof Cyborg);
+  //console.log(jonny instanceof Hybrid); //TypeError
+  Hybrid.prototype = Human.prototype;
+  console.log(jonny instanceof Hybrid); //true 
+}());
+
+(()=>{
+  /*  TAG 12 */
+  'use strict'
+  class Human{
+    constructor(name) {
+      this._name = name;
+    }
+  }
+  const molly = new Human("Molly");
+  console.log(Human instanceof Function); //A Class is an instance of Function
+  console.log(molly instanceof Human); //Moly is an instance of Human
+  console.log(Human.prototype instanceof Object); //The prototype property of a class is an Object
+  console.log(Human === Human.prototype.constructor); //A Class is its own prototype.constructor
+  console.log(Object.getPrototypeOf(Human) === Function.prototype); //[[Prototype]] of Human is Function.prototype
+  console.log(Object.getPrototypeOf(Human.prototype) === Object.prototype); //[[Prototype]] of Human.prototype is Object.prototype
+  console.log(Object.getPrototypeOf(molly) === Human.prototype); //[[Prototype]] of molly is Human.prototype
+
+}());
--- js/examples/temp.js
+++ js/examples/temp.js
@@ -1,16 +1,28 @@
 (()=>{
-  /*  TAG 6 */
+  /*  TAG 13 */
   'use strict'
-
-  class Human {
-    constructor(name){
+  class Human{
+    constructor(name) {
       this._name = name;
     }
-
-    ['id'+'entify']() {
-      return `Hello, I'm ${this._name}`;
+  }
+  class Cyborg extends Human{
+    constructor(name, enhancement){
+      super(name);
+      this._enhancement = enhancement;
     }
   }
-  const rei = new Human('Rei Toei');
-  console.log(rei.identify());
+  const molly = new Cyborg("Molly", "Hypervision");
+  console.log(Human instanceof Function); 
+  console.log(molly instanceof Human); 
+  console.log(molly instanceof Cyborg);
+  console.log(Human.prototype instanceof Object); 
+  console.log(Cyborg.prototype instanceof Human);
+  console.log(Human === Human.prototype.constructor);
+  console.log(Cyborg === Cyborg.prototype.constructor);
+  console.log(Object.getPrototypeOf(Cyborg) === Human);
+  console.log(Object.getPrototypeOf(Human) === Function.prototype);
+  console.log(Object.getPrototypeOf(molly) === Cyborg.prototype);
+  console.log(Object.getPrototypeOf(Cyborg.prototype) === Human.prototype);
+  console.log(Object.getPrototypeOf(Human.prototype) === Object.prototype);
 }());
